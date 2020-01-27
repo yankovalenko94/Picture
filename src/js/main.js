37aab0c21fc85bd8d466f1c99b7943d791ab21b4
Ivan <ivan-kerch@mail.ru>
2020-01-27 16:31:41 +0300

    the forms module is finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import modals from './modules/modals';
 import sliders from './modules/sliders';
+import forms from './modules/forms';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -7,4 +8,5 @@ window.addEventListener('DOMContentLoaded', () => {
     modals();
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
+    forms();
 });
