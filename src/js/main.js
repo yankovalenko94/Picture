43657f150d9a60b97c24d971891cfa9822f1ddd9
Ivan <ivan-kerch@mail.ru>
2020-01-27 17:34:03 +0300

    the mask module is finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import modals from './modules/modals';
 import sliders from './modules/sliders';
 import forms from './modules/forms';
+import mask from './modules/mask';
+import checkTextInputs from './modules/checkTextInputs';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -9,4 +11,7 @@ window.addEventListener('DOMContentLoaded', () => {
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
     forms();
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
 });
